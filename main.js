9bdf33487378c160ae54e26d604508d632e278fe
emranweb <emranbd77@gmail.com>
2020-10-24 23:24:25 +0600

    change

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hi")
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+
+const { world } = engine;
+
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+  },
+});
+
+
+
+Render.run(render);
+Render.run(Runner.create(), engine);
